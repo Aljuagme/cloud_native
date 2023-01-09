0480bdc88476b91569b4090354fd0fe9b26f5a70
Alvaro <aljuagme@gmail.com>
2023-01-09 21:13:45 +0100

    from start

--- k3d-start-cluster.sh
+++ k3d-start-cluster.sh
@@ -6,9 +6,15 @@ kubectl create namespace argocd
 # install argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# Forward?
+kubectl port-forward -n argocd svc/argocd-server 8080:443 &
+
 # the argo-cd intial admin password can be read with the following command:
+printf "\n"
+echo "PASSWORD FOR ARGOCD: "
 kubectl get secret argocd-initial-admin-secret -n argocd -o yaml
-# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+printf "\n\n"
 # OR
 # replace it with your own fixed admin password
 # https://www.browserling.com/tools/bcrypt
@@ -18,4 +24,6 @@ kubectl -n argocd patch secret argocd-secret -p '{"stringData": {"admin.password
 # start the GitOps with creation of argo-cd app
 kubectl -n argocd apply -f ./argo-cd/argo-cd-app.yaml
 
-kubectl port-forward -n argocd svc/argocd-server 8080:443
+
+
+
